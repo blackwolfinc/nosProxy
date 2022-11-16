158f4b5b61f43421c60f5e59ff032cdfab05089e
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-11-17 01:55:44 +0700

    fixed

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../img/common/Logo.png";
 
 export const Header = () => {
   return (
-    <div className="flex w-full justify-between px-[3rem] py-[4rem] absolute top-0">
+    <div className="flex w-full justify-between px-[3rem] py-[3rem] absolute top-0">
       <div>
         <img src={Logo} className={"w-[13rem]"} />
       </div>
