39c16e2fc962858fc723287f8f0e0602518144f9
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-11-17 08:26:50 +0700

    add progres

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../img/common/Logo.png";
 
 export const Header = () => {
+  const [Count, setCount] = useState(0);
+
+  const counter = (minimum, maximum) => {
+    for (let count = minimum; count <= maximum; count++) {
+      setTimeout(() => {
+        setCount(count);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    counter(0, 53536);
+  }, []);
+
   return (
     <div className="flex w-full justify-between px-[3rem] py-[3rem] absolute top-0">
       <div>
@@ -10,7 +24,7 @@ export const Header = () => {
 
       <div>
         <span className="counting text-[#fff]  tracking-[10px] text-7xl px-2">
-          53536
+          {Count}
         </span>
         <span className="counting  text-[#fff] text-lg">+IPS</span>
       </div>
--- src/assets/page/App.js
+++ src/assets/page/App.js
@@ -3,20 +3,34 @@ import Logo from "../img/common/Logo.png";
 import MoonIcon from "../img/common/Moon.png";
 import NextButton from "../img/icon/Login.png";
 import Rocket from "../img/common/Rocket.png";
+import Typewriter from "typewriter-effect";
+import { useEffect, useState } from "react";
+
 function App() {
   return (
     <div className="Homepage w-full overflow-hidden relative h-screen flex flex-col justify-center">
       <Header />
-      <div className="w-full z-20 flex justify-center mt-[7rem] relative items-center">
+      <div className="w-full z-20 flex justify-center mt-[7rem] relative PopUPAnimation items-center">
         <div className="bg-[#FF8311] w-[10rem] h-[0.4rem] text-[#FF8311]"></div>
         <img src={Logo} alt="" className="w-[25rem] " />
         <div className="bg-[#FF8311] ml-[1rem] h-[0.4rem] w-[10rem] text-[#FF8311]"></div>
       </div>
-      <div className="z-20 text-center text-[#fff] counting text-xl mt-[1rem]">
-        Constantly Growing Network
+      <div className="z-20 text-center text-[#fff] PopUPAnimation counting text-xl mt-[1rem]">
+        <Typewriter
+          options={{
+            strings: [
+              "Constantly Growing Network",
+              "Fastest Proxy Ever",
+              "Global Presence",
+            ],
+            delay: 200,
+            autoStart: true,
+            loop: true,
+          }}
+        />
       </div>
-      <div className="z-20 text-center text-[#fff] counting flex justify-center text-xl mt-[3rem]">
-        <button className="bg-[#323639]/80 px-[4rem] flex  items-center space-x-2 textbutton py-[0.7rem] rounded-[52.11px] border-[#78818A] border-[2px]">
+      <div className="z-20 text-center text-[#fff] counting flex justify-center text-xl PopUPAnimation mt-[3rem]">
+        <button className="bg-[#323639]/80 px-[4rem] flex  items-center hover:scale-105  space-x-2 textbutton py-[0.7rem] hover:border-[#FF8311] duration-300	 rounded-[52.11px] border-[#78818A] border-[2px]">
           <span>LOGIN OR JOIN</span>
           <img src={NextButton} alt=""></img>
         </button>
